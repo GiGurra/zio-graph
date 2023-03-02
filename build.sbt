973271a3b5783ea5ae677e4ffa6da82c1bd6536d
Johan Kjölhede <gigurra@gmail.com>
2023-03-02 20:05:54 +0100

    wip

--- build.sbt
+++ build.sbt
@@ -1,7 +1,8 @@
 scalaVersion := "3.2.0"
-version := "0.1.0"
-name := "zio-graph"
+version      := "0.1.0"
+name         := "zio-graph"
 
-libraryDependencies += "dev.zio" %% "zio" % "2.0.9"
-libraryDependencies += "dev.zio" %% "zio-logging-slf4j2" % "2.1.10"
-libraryDependencies += "dev.zio" %% "zio-json" % "0.4.2"
+libraryDependencies += "dev.zio"            %% "zio"                % "2.0.9"
+libraryDependencies += "dev.zio"            %% "zio-logging-slf4j2" % "2.1.10"
+libraryDependencies += "dev.zio"            %% "zio-json"           % "0.4.2"
+libraryDependencies += "io.github.iltotore" %% "iron"               % "2.0.0"
--- src/main/scala/com/github/gigurra/ziograph/MainWip.scala
+++ src/main/scala/com/github/gigurra/ziograph/MainWip.scala
@@ -1,5 +1,8 @@
 package com.github.gigurra.ziograph
 
+import io.github.iltotore.iron.*
+import io.github.iltotore.iron.constraint.numeric.{Greater, Less, Positive}
+import io.github.iltotore.iron.compileTime.stringValue
 import zio.json.*
 import zio.logging.{LogFormat, console, consoleJson}
 import zio.{ExitCode, IO, Runtime, Scope, Task, UIO, ZIO, ZIOAppArgs, ZIOAppDefault, ZLayer}
@@ -7,11 +10,33 @@ import zio.{ExitCode, IO, Runtime, Scope, Task, UIO, ZIO, ZIOAppArgs, ZIOAppDefa
 import java.util.concurrent.TimeUnit
 import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
+import scala.compiletime.constValue
 
 object MainWip extends ZIOAppDefault:
 
   override val bootstrap: ZLayer[ZIOAppArgs, Any, Any] =
     Runtime.removeDefaultLoggers >>> console(LogFormat.colored)
+
+  type SomeAge = Positive DescribedAs "Age should be positive"
+
+  final class Boogoo[V]
+  inline given [V <: Int]: Constraint[Int, Boogoo[V]] with
+    override inline def test(value: Int): Boolean = value + 1 > constValue[V]
+    override inline def message: String           = "Should be greater than " + stringValue[V]
+
+  def foo(
+      x: Int :| (Greater[0] & Less[12])
+  ): Unit = ???
+
+  def bar(
+      x: Int :| (Boogoo[1] & SomeAge)
+  ): Unit = ???
+
+  def foobar(
+      x: Int :| (Greater[0] & Less[12] & Boogoo[1] & SomeAge)
+  ): Unit =
+    foo(x)
+    bar(x)
 
   override def run: ZIO[Any, Any, Any] =
     (for {
